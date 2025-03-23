35280765ca3042372b0f669659409ab2c52f1489
vinicius daniel <166657297+vini-devizin@users.noreply.github.com>
2025-03-23 15:55:20 -0300

    Add: alacritty and tmux configs added

---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ls ~/ | grep -q "alacritty"; then
+	if ! ls ~/.config/ | grep -q "alacritty"; then
+		mv ~/alacritty/ ~/.config/
+		echo "Diretório ~/.config/alacritty/ criado"
+		echo "alacritty.yml movido para o diretório ~/.config/alacritty"
+		echo "Pasta themes movida para o diretório ~/.config/alacritty"
+	else
+		mv ~/alacritty/ ~/.config/
+		echo "Diretório ~/.config/alacritty/ já existente"
+		echo "alacritty.yml movido para o diretório ~/.config/alacritty"
+		echo "Pasta themes movida para o diretório ~/.config/alacritty"
+	fi
+elif ls ~/.config/alacritty/ | grep -q "alacritty.yml"; then
+	echo "alacritty.yml já está no seu devido lugar"
+else
+	echo "Arquivo alacritty.yml não encontrado"
+fi
