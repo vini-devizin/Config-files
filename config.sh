c8a4c7ab0e14c0385d3df9373da2b6e572ce52a0
vinicius daniel <166657297+vini-devizin@users.noreply.github.com>
2025-03-23 16:21:16 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if ls ~/ | grep -q "alacritty"; then
+if ls ~/ | grep -q "Config-files"; then
 	if ! ls ~/.config/ | grep -q "alacritty"; then
-		mv ~/alacritty/ ~/.config/
+		mv ~/Config-files/alacritty/ ~/.config/
 		echo "Diretório ~/.config/alacritty/ criado"
 		echo "alacritty.yml movido para o diretório ~/.config/alacritty"
 		echo "Pasta themes movida para o diretório ~/.config/alacritty"
 	else
-		mv ~/alacritty/ ~/.config/
+		mv ~/Config-files/alacritty/ ~/.config/
 		echo "Diretório ~/.config/alacritty/ já existente"
 		echo "alacritty.yml movido para o diretório ~/.config/alacritty"
 		echo "Pasta themes movida para o diretório ~/.config/alacritty"
